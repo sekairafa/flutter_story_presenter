dc420ea2fcb523baa72adca3ef2221841277786c
Kevin Piazzoli Felix perez <kevinpiazzolifelixperez@Mac-mini-de-Kevin.local>
2025-04-23 12:53:06 -0500

    new inappwebview

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
-import flutter_inappwebview_macos
 import just_audio
 import path_provider_foundation
 import sqflite
 import video_player_avfoundation
+import zikzak_inappwebview_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
 }
